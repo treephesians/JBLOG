01938e5bd59bca152c409db5af2a249ab86c2ff8
Junbeom Park <136706252+treephesians@users.noreply.github.com>
2025-02-09 23:42:19 +0900

    feat: add dark mode (#3)
    * feat: add npm shadcn ui
    
    * feat: add header, dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          This is chore/ci-auto-pr
         </a>
       </footer>
     </div>
