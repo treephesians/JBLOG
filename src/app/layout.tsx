092e1c88c99e5e631fb151f8bf7f02796ad7305a
Junbeom Park <ephesi4ns623@gmail.com>
2025-04-11 15:34:35 +0900

    fix: fix issue with back jesture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col overscroll-none`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
         <ThemeProvider
           attribute="class"
